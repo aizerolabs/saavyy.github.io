825798cbef5a88d297160a60111dd67751c8ef0c
Aizero Technologies <75944451+anaizero@users.noreply.github.com>
2024-10-31 19:10:25 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 $(document).ready(function() {
 	"use strict";
 	$("#countdown").countdown({
-		date: "14 January 2024 9:00:00", /** Enter new date here **/
+		date: "14 January 2025 9:00:00", /** Enter new date here **/
 		format: "on"
 	},
 	function() {
